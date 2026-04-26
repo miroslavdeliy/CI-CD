3ecf1700f93c2aa96229e8da78045e915d78f82d
Miroslav Dely <miroslavdely@gmail.com>
2026-04-26 22:54:05 +0300

    Fix code with ruff

--- app/fix_me.py
+++ app/fix_me.py
@@ -1,31 +1,22 @@
-def max_number(a,b):
-
-   if a>b :
-
-   return a
-
+def max_number(a, b):
+    if a > b:
+        return a
     else:
-
-    return b
-
-
-def multiply (a,b):
-
-       return a    *    b
+        return b
 
 
-class  FixMe:
+def multiply(a, b):
+    return a * b
 
-   def __init__ (self,name):
 
-       self.name=name
+class FixMe:
+    def __init__(self, name):
+        self.name = name
 
-   def zero_function(x,y):
-
-         if x>0: print("positive")
-
-         elif x<0:
-
-          print("negative")
-
-         else: print("zero")
+    def zero_function(self, x, y):
+        if x > 0:
+            print("positive")
+        elif x < 0:
+            print("negative")
+        else:
+            print("zero")
