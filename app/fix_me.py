bb51c2503ce6d69a2de947c7cc3810c108d5691e
Miroslav Dely <miroslavdely@gmail.com>
2026-04-26 22:36:19 +0300

    Add broken code

---
+++ app/fix_me.py
@@ -0,0 +1,31 @@
+def max_number(a,b):
+
+   if a>b :
+
+   return a
+
+    else:
+
+    return b
+
+
+def multiply (a,b):
+
+       return a    *    b
+
+
+class  FixMe:
+
+   def __init__ (self,name):
+
+       self.name=name
+
+   def zero_function(x,y):
+
+         if x>0: print("positive")
+
+         elif x<0:
+
+          print("negative")
+
+         else: print("zero")
